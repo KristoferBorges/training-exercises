c04af943cdf566818af4f309d58b7af1e2e35551
KristoferBorges <kristofer.souza2027@hotmail.com>
2023-12-11 14:42:08 -0800

    Novos desafios de C# serão desenvolvidos

---
+++ Exercícios/Scripts_C#/ScriptsUsandoC#/ScriptsUsandoC#/Desafio_01.cs
@@ -0,0 +1,9 @@
+﻿// Crie um script python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de arcodo com o valor digitado.
+
+class BemVindo
+{
+    static void Main()
+    {
+        
+    }
+}
---
+++ Exercícios/Scripts_C#/ScriptsUsandoC#/ScriptsUsandoC#/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program
+{
+    private static void Main1(string[] args)
+    {
+        Console.WriteLine("Bom dia familia");
+    }
+}
