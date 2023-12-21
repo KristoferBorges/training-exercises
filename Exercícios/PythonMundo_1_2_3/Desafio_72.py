33b930323f8283ef38295d97f008f0f2796a8c76
KristoferBorges <kristofer.souza2027@hotmail.com>
2023-12-20 21:31:55 -0800

    new challenge

---
+++ Exercícios/PythonMundo_1_2_3/Desafio_72.py
@@ -0,0 +1,27 @@
+def numerosPorExtenso(number):
+    numeros = tuple(["Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Onze", "Doze", "Treze", "Catorze", "Quinze", "Dezesseis", "Dezessete", "Dezoito", "Dezenove", "Vinte"])
+
+    while True:
+        try:
+            number = int(number)
+
+            if number > 20 or number < 0:
+                print("[!] - APENAS NÚMEROS DE 0 A 20")
+                number = int(input("INFORME UM NÚMERO: "))
+            else:
+                print(f"VOCÊ DIGITOU O NÚMERO [{numeros[number]}]")
+                break
+        except ValueError:
+            print("[!] - O VALOR NÃO PODE SER DIFERENTE DE UM NÚMERO")
+            number = int(input("INFORME UM NÚMERO: "))
+            continue
+        except Exception as error:
+            print("[!] - APENAS NÚMEROS DE 0 A 20")
+            print(error)
+            number = int(input("INFORME UM NÚMERO: "))
+        
+            
+   
+
+numberInput = input("INFORME UM NÚMERO: ")
+numerosPorExtenso(numberInput)
